1615a426e52ce21c988d049348ef67c476cdca4d
anisaekaputrii00-star <anisaekaputri00@gmail.com>
2026-03-30 20:16:58 +0700

    masukan nilai matematika dan bahasa inggris

--- looping.cpp
+++ looping.cpp
@@ -40,3 +40,7 @@ int main() {
 int main(){
     float nilB,nilM,rerata;
     string status;
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
