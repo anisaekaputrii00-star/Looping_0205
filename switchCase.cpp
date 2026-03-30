4e065a3d05c1ce8cce5dd0a01bf716b2db6d9a8e
anisaekaputrii00-star <anisaekaputri00@gmail.com>
2026-03-30 20:45:37 +0700

    hasil seleksi

--- switchCase.cpp
+++ switchCase.cpp
@@ -37,3 +37,12 @@ void pilihanJurusan() {
             break;
     }
 }
+
+// prosedur
+void output() {
+    cout << "\n=== HASIL SELEKSI ===" << endl;
+    cout << "Jurusan Pilihan : " << namaJurusan << endl;
+    cout << "Status Kuota    : " << statusKuota << endl;
+    cout << "----------------------------------" << endl;
+}
+
